7201ab260a1b5fe87419a790845dddf7d6970acb
Ivan Knezić <37369060+occamsrazor81@users.noreply.github.com>
2021-01-10 20:27:46 +0100

    add areas

---
+++ Music/Areas/Identity/Data/MusicUser.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+
+namespace Music.Areas.Identity.Data
+{
+    // Add profile data for application users by adding properties to the MusicUser class
+    public class MusicUser : IdentityUser
+    {
+        [PersonalData]
+        [Column(TypeName = "nvarchar(100)")]
+        public string FirstName { get; set; }
+
+        [PersonalData]
+        [Column(TypeName = "nvarchar(100)")]
+        public string LastName { get; set; }
+    }
+
+    // current users:
+    // 1: stvar.principa@gmail.com - 123456*
+    // 2: abx@example.com - 12345678*
+}
