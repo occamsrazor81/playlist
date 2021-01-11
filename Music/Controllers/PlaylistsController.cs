d8131bddccc699acaecf9355891ffbe62101a0a4
Ivan Knezić <37369060+occamsrazor81@users.noreply.github.com>
2021-01-11 21:02:47 +0100

    update controllers (adding playlists), viewmodels

--- Music/Controllers/PlaylistsController.cs
+++ Music/Controllers/PlaylistsController.cs
@@ -1,9 +1,12 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore;
 using Music.Areas.Identity.Data;
 using Music.Data;
 using Music.Models;
+using Music.ViewModels;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -33,5 +36,77 @@ namespace Music.Controllers
                                 
             return View(myPlaylists);
         }
+
+        [Authorize]
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [Authorize]
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(string title)
+        {
+            if (ModelState.IsValid)
+            {
+                var userId = User.FindFirstValue(ClaimTypes.NameIdentifier);
+                Playlist playlistObj = new Playlist();
+                playlistObj.MusicUserId = userId;
+                playlistObj.Title = title;
+
+                await _db.Playlists.AddAsync(playlistObj);
+                await _db.SaveChangesAsync();
+
+                return RedirectToAction("Index");
+            }
+
+            else return View();
+        }
+
+        [Authorize]
+        [HttpGet]
+        public async Task<IActionResult> EditPlaylist(int? id)
+        {
+            if (id == null) return NotFound();
+            else
+            {
+                Playlist playlistObj = await _db.Playlists.FindAsync(id);
+                if (playlistObj == null) return NotFound();
+                else
+                {
+                    List<PlaylistSong> playlistSongs = await _db
+                        .PlaylistSongs
+                        .Include(s => s.Song)
+                        .Where(ps => ps.PlaylistId == id)
+                        .ToListAsync();
+
+                    playlistObj.PlaylistSongs = playlistSongs;
+                
+                    return View(playlistObj);
+                }
+            }
+        }
+
+        [Authorize]
+        [HttpGet]
+        public async Task<IActionResult> AddSongs(int? id)
+        {
+            if (id == null) return NotFound();
+
+            Playlist playlistObj = await _db.Playlists.FindAsync(id);
+
+            SqlParameter idParam = new SqlParameter("@PlaylistId", id);
+
+            List<Song> remainingSongs = await _db
+                .Songs
+                .FromSqlRaw("select * from dbo.Songs where Id not in" +
+                "(select SongId from dbo.PlaylistSongs where PlaylistId = @PlaylistId)", idParam)
+                .ToListAsync();
+
+            return View(new AddSongsToPlaylistViewModel(playlistObj, remainingSongs));
+        }
+
     }
 }
---
+++ Music/ViewModels/AddSongsToPlaylistViewModel.cs
@@ -0,0 +1,26 @@
+﻿using Music.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Music.ViewModels
+{
+    public class AddSongsToPlaylistViewModel
+    {
+        public Playlist Playlist { get; set; }
+
+        public List<Song> RemainingSongs { get; set; }
+
+        public List<string> SongsToAddIds;
+
+        public AddSongsToPlaylistViewModel() { }
+
+        public AddSongsToPlaylistViewModel(Playlist p, List<Song> remSongs) 
+        {
+            Playlist = p;
+            RemainingSongs = remSongs;
+            SongsToAddIds = new List<string>(new string[remSongs.Count]);
+        }
+    }
+}
