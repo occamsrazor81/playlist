1da8e13a03581cbb4f503ddb53c7b3bfb83e6901
Ivan Knezić <37369060+occamsrazor81@users.noreply.github.com>
2021-01-23 20:49:33 +0100

    different view models

---
+++ Music/ViewModels/UserPlaylistViewModel.cs
@@ -0,0 +1,36 @@
+﻿using Music.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Music.ViewModels
+{
+    public class UserPlaylistViewModel
+    {
+
+        // 1. playlist
+        public Playlist Playlist { get; set; }
+
+        // 2. songs in playlist
+        public List<Song> PlaylistSongs { get; set; }
+
+        // 3. user data
+        public string UserId { get; set; }
+        public string UserFirstName { get; set; }
+        public string UserLastName { get; set; }
+        public string UserEmail { get; set; }
+
+        public UserPlaylistViewModel() { }
+        public UserPlaylistViewModel(Playlist p, List<Song> sgs, string uId, string fname, string lname, string email)
+        {
+            Playlist = p;
+            PlaylistSongs = sgs;
+            UserId = uId;
+            UserFirstName = fname;
+            UserLastName = lname;
+            UserEmail = email;
+
+        }
+    }
+}
---
+++ Music/ViewModels/UserViewModel.cs
@@ -0,0 +1,29 @@
+﻿using Music.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Music.ViewModels
+{
+    public class UserViewModel
+    {
+        public string Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+
+        public List<Playlist> Playlists { get; set; }
+
+        public UserViewModel() { }
+
+        public UserViewModel(string id, string fname, string lname, string email)
+        {
+            Id = id;
+            FirstName = fname;
+            LastName = lname;
+            Email = email;
+        }
+
+    }
+}
