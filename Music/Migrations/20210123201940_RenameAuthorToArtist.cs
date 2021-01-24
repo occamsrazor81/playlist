0d10f107a3aaccbbc9b8a8fc11c7852215e38043
Ivan Knezić <37369060+occamsrazor81@users.noreply.github.com>
2021-01-24 10:39:30 +0100

    author -> artist

---
+++ Music/Migrations/20210123201940_RenameAuthorToArtist.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Music.Migrations
+{
+    public partial class RenameAuthorToArtist : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Author",
+                table: "Songs",
+                newName: "Artist");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Artist",
+                table: "Songs",
+                newName: "Author");
+        }
+    }
+}
