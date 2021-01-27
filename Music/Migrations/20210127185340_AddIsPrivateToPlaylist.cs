200789402f1645d0f37e6a07c2f9c35d308a37d8
Ivan Knezić <37369060+occamsrazor81@users.noreply.github.com>
2021-01-27 21:47:02 +0100

    migration to add private

---
+++ Music/Migrations/20210127185340_AddIsPrivateToPlaylist.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Music.Migrations
+{
+    public partial class AddIsPrivateToPlaylist : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "isPrivate",
+                table: "Playlists",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "isPrivate",
+                table: "Playlists");
+        }
+    }
+}
