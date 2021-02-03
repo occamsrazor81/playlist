a7692791489817d4250c5328e60a87d1c90055e4
Ivan Knezić <37369060+occamsrazor81@users.noreply.github.com>
2021-02-03 20:23:05 +0100

    viewmodel for similar users

---
+++ Music/ViewModels/SimilarUsersViewModel.cs
@@ -0,0 +1,24 @@
+﻿using Music.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Music.ViewModels
+{
+    public class SimilarUsersViewModel
+    {
+        public UserViewModel UserData { get; set; }
+        public int PlaylistId { get; set; }
+
+        public List<Song> SongsInCommon { get; set; }
+
+        public SimilarUsersViewModel() {}
+        public SimilarUsersViewModel(int pId, UserViewModel uvm, List<Song> songList)
+        {
+            PlaylistId = pId;
+            UserData = uvm;
+            SongsInCommon = songList;
+        }
+    }
+}
