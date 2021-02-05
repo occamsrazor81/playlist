a68f0b64fc2e997f237a9e9663bf6245d7e79325
Ivan Knezić <37369060+occamsrazor81@users.noreply.github.com>
2021-02-05 13:48:41 +0100

    send email vm

---
+++ Music/ViewModels/SendEmailViewModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Music.ViewModels
+{
+    public class SendEmailViewModel
+    {
+        
+        public string RecepientId { get; set; }
+        public string EmailFrom { get; set; }
+        public string EmailTo { get; set; }
+        public string Subject { get; set; }
+        public string Body { get; set; }
+
+        public SendEmailViewModel() {}
+        public SendEmailViewModel(string recepientId , string from, string to)
+        {
+            RecepientId = recepientId;
+            EmailFrom = from;
+            EmailTo = to;
+        }
+    }
+}
